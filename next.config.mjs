8f6fbc07e812c9391792b84cea988a5581da9993
yanliang-code <1205836625@qq.com>
2025-12-17 11:09:23 +0800

    升级后打包错误问题处理

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
-}
+  webpack: (config) => {
+    config.resolve.alias ??= {};
+    for (const mod of [
+      "tap",
+      "tape",
+      "why-is-node-running",
+      "pino-pretty",
+      "desm",
+      "fastbench",
+      "pino-elasticsearch",
+    ]) {
+      config.resolve.alias[mod] = false;
+    }
+    return config;
+  },
+};
 
-export default nextConfig
+export default nextConfig;
